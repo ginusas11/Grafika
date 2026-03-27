01fd0502f95993c93609e5b89a8503dc5693006e
Tóth Beáta Georgina <tothbeatageorgina@gmail.com>
2026-03-27 13:39:25 +0100

    Circle

---
+++ demos/circle/circle.h
@@ -0,0 +1,56 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <SDL2/SDL.h>
+
+
+/**
+ * Data of a circle object in Descartes coordinate system
+ */
+typedef struct Color{
+	Uint8 r, g, b;
+} Color;
+
+typedef struct Circle
+{
+	double x;
+	double y;
+	double radius;
+	Color color;
+
+	int steps;
+	double angle_step;
+	double max_segment_length;
+} Circle;
+
+typedef struct Screen{
+	int width;
+	int height;
+} Screen;
+
+typedef struct Point {
+  int x;
+  int y;
+} Point;
+
+typedef enum{
+	MODE_STEP_COUNT,
+	MODE_ANGLE_STEP,
+	MODE_MAX_SEGMENT
+} MODE;
+
+/**
+ * Set the data of the circle
+ */
+void set_circle_data(Circle* circle, double x, double y, double radius, Color c, int s, double a_s, double m_s_l);
+
+void draw_circle(Screen* screen, Circle* circle, MODE mode);
+/**
+ * Calculate the area of the circle.
+ */
+double calc_circle_area(const Circle* circle);
+
+#endif // CIRCLE_H
---
+++ demos/circle/main.c
@@ -0,0 +1,12 @@
+#include "circle.h"
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{	
+	SDL_Init(SDL_INIT_VIDEO);
+	
+	
+
+	return 0;
+}
